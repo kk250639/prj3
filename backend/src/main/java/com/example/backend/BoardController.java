4c932698333f40e25c35e6910999442d6b3d3437
kk250639 <chesyeo@gmail.com>
2025-07-15 16:51:25 +0900

    first commit

---
+++ backend/src/main/java/com/example/backend/BoardController.java
@@ -0,0 +1,11 @@
+package com.example.backend;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+
+}
